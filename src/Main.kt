f6c151579989171bd5386ceaddf514f948c2cd91
waimea-gkidd <gekidd@waimea.school.nz>
2025-03-18 14:47:25 +1300

    description

--- src/Main.kt
+++ src/Main.kt
@@ -4,7 +4,7 @@
  * ---------------------------------------------------------------------
  * Project Name:   PROJECT NAME HERE
  * Project Author: PROJECT AUTHOR HERE
- * GitHub Repo:    GITHUB REPO URL HERE
+ * GitHub Repo:    https://github.com/waimea-gkidd/evel-2-programming-assessment
  * ---------------------------------------------------------------------
  * Notes:
  * PROJECT NOTES HERE

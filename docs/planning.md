43a7644ac383f4c8de11e65ece23686cdf8af862
waimea-gkidd <gekidd@waimea.school.nz>
2025-03-18 15:08:22 +1300

    planning

---
+++ docs/planning.md
@@ -0,0 +1,13 @@
+# Planning
+
+## Things I need to 
+
+- jghj
+- hhlo
+- jh;
+
+1. hjghkj
+2. j;kj
+3. ]hj'l
+
+
--- src/Main.kt
+++ src/Main.kt
@@ -7,7 +7,14 @@
  * GitHub Repo:    https://github.com/waimea-gkidd/evel-2-programming-assessment
  * ---------------------------------------------------------------------
  * Notes:
- * PROJECT NOTES HERE
+ * **Old gold is a game for two players which consists of 8 coins as well as one gold with 20 places which may hold no more than one coin at a time.
+ * The aim is to be the one to get the gold coin out of the game.
+ * The coins are placed randomly between these 20 squares.
+ * The coins can only move right to left providing there is not another coin in the way (you can not go through other coins)
+ * You remove coins by moving them to the last square to the left;
+ * if a coin is already on the last square and it's your turn you can decide whether to remove it from the game or not-
+ * same goes for the 2nd player for removing a coin.
+ * Although you get the choice whether or not to remover a coin if you have no other moves you must.**
  * =====================================================================
  */
 

a3bfeb3684df0e8b2a9e580db140d2c7c0c3b3b5
waimea-gkidd <gekidd@waimea.school.nz>
2025-03-31 10:59:26 +1300

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -22,7 +22,6 @@
 
 const val coinCages = 20
 const val EMPTY = " "
-const val NUMCOINS = 9
 fun main() {
     println("Welcome to Old Gold.")
     println("--------------------")
@@ -31,6 +30,11 @@ fun main() {
 //    listPlayers(playerNum)
 //    println()
 //    setPlayerNum.add //create a collection that can be called for listing which players turn it is as well as making them player 1 or 2
+
+
+
+    // to do: use a remove val and a "Left" and/or right movement to use player input
+
 
     println("In old gold there are only two players")
 
@@ -47,7 +51,7 @@ fun main() {
     val coins = mutableListOf<String>()
     val goldCoin = "GC" //0 {getString("There is one gold coin.")}
     val silverCoins = List(8) { "SC" }// 8 silver coins
-    val spaces = List(11) { "  " }// 8 silver coins
+    val spaces = List(11) { "  " }
     coins.add(goldCoin)
     coins.addAll(silverCoins) //add a function to filter through coins and place them randomly
     coins.addAll(spaces) //add a function to filter through coins and place them randomly
@@ -61,6 +65,8 @@ fun main() {
     println("")
     println("\rCoins have been set up.")
     val move = getPlayerMove("Enter which coin you would like to move: ") // create a string which is able
+    println(move) // create game update function
+
 
 }
 fun setupCoins(): MutableList<String> {
@@ -81,15 +87,15 @@ fun getString(prompt: String): String {
 fun gameLoop(prompt: String) {} //find code
 
 
-fun displayGame(coinPlacement: List<String>) {
+fun displayGame(coinCages: List<String>) {
 
 
 
-    val banner = ("----------".repeat(coinPlacement.size) + " ")
+    val banner = ("----------".repeat(coinCages.size) + " ")
 
     println(banner)
 
-    for(i in 0 ..<coinPlacement.size) {
+    for(i in 0 ..<coinCages.size) {
         print("| coin ${i + 1} " .padEnd(4))
     }
 
@@ -99,10 +105,39 @@ fun displayGame(coinPlacement: List<String>) {
 
 
 }
+
 fun getPlayerMove(prompt: String): String {
     print(prompt)
     return readln()
+//    println(readln())
 }
+
+
+/* to do:
+        -set a val for data storage and create a function to store user data and update the game at the same time
+        -set controls for removing coins as well as moving them left (also add barrier so that use cannot drag coins out of cages)
+        -check for a win (if so other player loses) and display a message for results with player names
+        -if gold coin is not removed the continue game and swap to other player
+
+        1. create a data storage function. Teacher tips: use arrays/ variables most if not all of which will be global
+            write a setupGame() method that puts sensible initial values in these variables, positions on board, ask user
+            for data (player names ect)
+        2. set controls. inside of player move function controls should be displayed and available to use through the use
+            of vals/vars controls consist of removing coins and moving coins to the left.
+        3. check for a win. If gold coin is removed then game ends and return results (probably stored in data function)
+        4. if gold coin is not removed then continue game swap player and repeat.
+        5. for game loop:  for this loop add the functions display, getPlayerInput, updateGame and check for win or loss
+        - continue to next player.
+        // Loop through the list - from collections
+    for (Snack in snacks) {
+        println(Snack)
+    }
+
+
+
+ */
+
+
 //    return move
 ////    create either a when statement to swap back and forth between players or create a new function for player 2
 //}
@@ -123,4 +158,3 @@ fun getPlayerMove(prompt: String): String {
 //    }
 //}
 //
-//fun dataStprage
